94ba1fea2c8f3b78850f4ae6a3841c6384249c6d
Artyom <sgnoteii55@gmail.com>
2024-02-13 18:57:15 +0400

    started chapter №5, completed BA5A

---
+++ №5. How Do We Compare DNA Sequences/BA5A.java
@@ -0,0 +1,58 @@
+import java.nio.file.Path;
+import java.util.*;
+
+public class BA5A {
+
+    private static void shiftList(List<Integer> list) {
+        list.removeFirst();
+        list.add(0);
+    }
+
+    private static int DPChangeMachinery(int money, int[] coins) {
+        int windowSize = coins[coins.length - 1], diff;
+        List<Integer> minNumCoinsWindow = new ArrayList<>(windowSize + 1);
+        for (int i = 0; i <= windowSize; ++i) {
+            minNumCoinsWindow.add(0);
+        }
+
+        for (int i = 1; i <= windowSize; ++i) {
+            minNumCoinsWindow.set(i, Integer.MAX_VALUE);
+            for (int coin : coins) {
+                if (i >= coin) {
+                    diff = i - coin;
+                    if (minNumCoinsWindow.get(diff) + 1 < minNumCoinsWindow.get(i)) {
+                        minNumCoinsWindow.set(i, minNumCoinsWindow.get(diff) + 1);
+                    }
+                }
+            }
+        }
+        shiftList(minNumCoinsWindow);
+        for (int i = windowSize + 1; i <= money; ++i) {
+            minNumCoinsWindow.set(windowSize, Integer.MAX_VALUE);
+            for (int coin : coins) {
+                diff = windowSize - coin;
+                if (minNumCoinsWindow.get(diff) + 1 < minNumCoinsWindow.get(windowSize)) {
+                    minNumCoinsWindow.set(windowSize, minNumCoinsWindow.get(diff) + 1);
+                }
+            }
+            shiftList(minNumCoinsWindow);
+        }
+
+        return minNumCoinsWindow.get(windowSize - 1);
+    }
+
+    public static int DPChange(Path path) {
+        List<String> strDataset = UTIL.readDataset(path);
+
+        return DPChangeMachinery(
+                Integer.parseInt(strDataset.getFirst()),
+                Arrays.stream(strDataset.getLast().split(","))
+                        .mapToInt(Integer::parseInt)
+                        .toArray()
+        );
+    }
+
+    public static int DPChange(int money, int[] coins) {
+        return DPChangeMachinery(money, coins);
+    }
+}
