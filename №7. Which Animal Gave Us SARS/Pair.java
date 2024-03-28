ed4cf8117b5ba44dd187808d729379319e199942
Artyom <sgnoteii55@gmail.com>
2024-03-28 22:41:31 +0400

    feat: pair data structure updated

---
+++ №7. Which Animal Gave Us SARS/Pair.java
@@ -0,0 +1,38 @@
+package auxil;
+
+public class Pair<T, V> {
+    private T first;
+    private V second;
+
+    public Pair(T first, V second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public V getSecond() {
+        return second;
+    }
+
+    public void setFirst(T first) {
+        this.first = first;
+    }
+
+    public void setSecond(V second) {
+        this.second = second;
+    }
+
+    public void setBoth(T first, V second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    @Override
+    public String toString() {
+        return "Pair{first=%s, second=%s}"
+                .formatted(first, second);
+    }
+}
