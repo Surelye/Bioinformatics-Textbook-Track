29ebdc1d40aeb5cac44bac27afc6df2855b84140
Artyom <sgnoteii55@gmail.com>
2024-04-10 22:54:39 +0400

    feat: BA9UTIL updated (construct modified suffix tree added)

--- №9. How Do We Locate Disease-Causing Mutations/BA9UTIL.java
+++ №9. How Do We Locate Disease-Causing Mutations/BA9UTIL.java
@@ -1,3 +1,8 @@
+import auxil.MSuffixTrie;
+import auxil.Node;
+import auxil.Edge;
+
+import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Stream;
 
@@ -9,5 +14,34 @@ public class BA9UTIL {
                 .stream()
                 .filter(entry -> value.equals(entry.getValue()))
                 .map(Map.Entry::getKey);
+    }
+
+    public static MSuffixTrie constructModifiedSuffixTrie(String text) {
+        int textLength = text.length(), index = 0;
+        char currentSymbol;
+        MSuffixTrie trie = new MSuffixTrie(new Node(0));
+        Node currentNode, endingNode, newNode;
+        Edge newEdge;
+
+        for (int i = 0; i != textLength; ++i) {
+            currentNode = new Node(0);
+            for (int j = i; j != textLength; ++j) {
+                currentSymbol = text.charAt(j);
+                endingNode = trie.hasLabeledEdge(currentNode, currentSymbol);
+                if (endingNode.isProper()) {
+                    currentNode = endingNode;
+                } else {
+                    newNode = new Node(++index);
+                    newEdge = new Edge(currentSymbol, j);
+                    trie.addEdge(currentNode, newNode, newEdge);
+                    currentNode = newNode;
+                }
+            }
+            if (trie.isLeaf(currentNode)) {
+                currentNode.setPosition(i);
+            }
+        }
+        System.out.println(trie);
+        return trie;
     }
 }
