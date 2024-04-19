48afec430dda427a32b81d74374ff817d3689189
Artyom <sgnoteii55@gmail.com>
2024-04-19 22:42:47 +0400

    feat: BA9F (find shortest non-shared substring) completed

---
+++ №9. How Do We Locate Disease-Causing Mutations/BA9F.java
@@ -0,0 +1,51 @@
+// Find the Shortest Non-Shared Substring of Two Strings
+// -----------------------------------------------------
+//
+// In “Find the Longest Repeat in a String” and “Find the Longest Substring Shared by Two Strings”,
+// we encountered two problems that could be solved using a suffix tree. A third such problem is
+// shown below.
+//
+// Shortest Non-Shared Substring Problem
+//
+// Find the shortest substring of one string that does not appear in another string.
+//
+// Given: Strings Text1 and Text2.
+//
+// Return: The shortest substring of Text1 that does not appear in Text2. (Multiple solutions may
+// exist, in which case you may return any one.)
+//
+// Sample Dataset
+// --------------
+// CCAAGCTGCTAGAGG
+// CATGCTGGGCTGGCT
+// --------------
+//
+// Sample Output
+// -------------
+// AA
+// -------------
+
+import java.nio.file.Path;
+
+public class BA9F {
+
+    private static String findShortestNonSharedSubstringMachinery(String f, String s) {
+
+    }
+
+    public static String findShortestNonSharedSubstring(String f, String s) {
+
+    }
+
+    public static String findShortestNonSharedSubstring(Path path) {
+
+    }
+
+    private void run() {
+
+    }
+
+    public static void main(String[] args) {
+        new BA9F().run();
+    }
+}
