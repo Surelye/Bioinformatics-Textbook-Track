b9c95e07f3abf89b8a9896afdad9f414cd66b2c9
Artyom <sgnoteii55@gmail.com>
2024-04-27 19:32:13 +0400

    feat: Symbol data structure updated

--- №9. How Do We Locate Disease-Causing Mutations/auxil/Symbol.java
+++ №9. How Do We Locate Disease-Causing Mutations/auxil/Symbol.java
@@ -1,9 +1,17 @@
 package auxil;
 
-public record Symbol(char symbol, int occurrence) {
+public record Symbol(char symbol, int occurrence) implements Comparable<Symbol> {
+
     @Override
     public String toString() {
-        return "Symb{s=%c, i=%d}"
-                .formatted(symbol, occurrence);
+        return "Symb{%c, i=%d}".formatted(symbol, occurrence);
+    }
+
+    @Override
+    public int compareTo(Symbol o) {
+        int res = Character.compare(symbol, o.symbol);
+        return (res == 0) ?
+                Integer.compare(occurrence, o.occurrence) :
+                res;
     }
 }
