2432a4ee3fb123ff6d377eafae3d8f254eb9fb0d
Artyom <sgnoteii55@gmail.com>
2024-05-02 00:05:44 +0400

    feat: BA10UTIL updated

--- №10. Why Have Biologists Still Not Developed an HIV Vaccine/BA10UTIL.java
+++ №10. Why Have Biologists Still Not Developed an HIV Vaccine/BA10UTIL.java
@@ -2,14 +2,29 @@ import auxil.HMM;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.IntStream;
 
 public class BA10UTIL {
 
     public static final String separator = "--------";
+
+    public static List<List<Double>> initDoubleMatrix(int fDim, int sDim) {
+        List<List<Double>> dMat = new ArrayList<>(fDim);
+        for (int i = 0; i != fDim; ++i) {
+            dMat.add(new ArrayList<>(Collections.nCopies(sDim, 0.)));
+        }
+        return dMat;
+    }
+
+    public static List<List<Double>> initDoubleMatrix(int dim) {
+        return initDoubleMatrix(dim, dim);
+    }
 
     public static List<Double> parseDoubleArray(String strDoubleArr, String regex) {
         return Arrays.stream(strDoubleArr.split(regex))
@@ -67,6 +82,14 @@ public class BA10UTIL {
         return Arrays.stream(strCharArr.split(regex))
                 .map(str -> str.charAt(0))
                 .toList();
+    }
+
+    public static Map<Character, Integer> parseCharacterArrayToMap(String strCharArr, String regex) {
+        List<Character> alphabet = parseCharacterArray(strCharArr, regex);
+        Map<Character, Integer> charToIndex = new HashMap<>(alphabet.size());
+        IntStream.range(0, alphabet.size())
+                .forEach(i -> charToIndex.put(alphabet.get(i), i));
+        return charToIndex;
     }
 
     public static HMM parseHMM(List<String> strHMM) {
